807c8c859978eb36aa899e74fa42752e6f8d28c3
Mariusz112 <maniek11744@gmail.com>
2022-07-18 12:09:14 +0200

    negatywne testy

--- RealWorldAPI.Test/UserServiceTests.cs
+++ RealWorldAPI.Test/UserServiceTests.cs
@@ -50,11 +50,10 @@ namespace RealWorldAPI.Test
             Mock<IMapper> mockMapper = new Mock<IMapper>();
             mockMapper.Setup(x => x.Map<UserResponse>(It.IsAny<User>())).Returns(response);
 
-            //Mock<UserManager<User>> userManager = GetMockUserManager();
-            //userManager.Setup(x => x.CreateAsync(It.IsAny<User>(), It.IsAny<string>())).ReturnsAsync(IdentityResult.Success);
+            
             string hashPassword = string.Empty;
 
-            //to jest ca³kowicie to zmiany
+           
             Mock<IPasswordHasher<User>> passwordHasher = new Mock<IPasswordHasher<User>>();
             passwordHasher.Setup(x => x.HashPassword(It.IsAny<User>(), It.IsAny<string>())).Returns(hashPassword);
 
@@ -69,6 +68,55 @@ namespace RealWorldAPI.Test
             Assert.IsNotNull(AddedUser);
             mockMapper.Verify(x => x.Map<UserResponse>(It.IsAny<User>()), Times.Once());
             Assert.That(AddedUser.User.Username, Is.EqualTo(expected.User.Username));
+
+        }
+        [Test]
+        public async Task UserRegister_WithInCorrectData_Test()
+        {
+            //ARRANGE
+            UserRegister user = new UserRegister()
+            {
+                Email = "Test1",
+                Password = "Test2",
+                Username = "Test3"
+            };
+
+            UserResponse response = new UserResponse()
+            {
+                Email = "Test1",
+                Username = "Test3"
+            };
+            UserResponseContainer expected = new UserResponseContainer()
+            {
+                User = new UserResponse
+                {
+                    Email = "Tesf43f3t1",
+                    Username = "rvfrewvf3wf"
+                }
+            };
+
+            //mockup
+            Mock<IMapper> mockMapper = new Mock<IMapper>();
+            mockMapper.Setup(x => x.Map<UserResponse>(It.IsAny<User>())).Returns(response);
+
+
+            string hashPassword = string.Empty;
+
+
+            Mock<IPasswordHasher<User>> passwordHasher = new Mock<IPasswordHasher<User>>();
+            passwordHasher.Setup(x => x.HashPassword(It.IsAny<User>(), It.IsAny<string>())).Returns(hashPassword);
+
+
+            Mock<IUserRepositorie> mockRepostiory = new Mock<IUserRepositorie>();
+            mockRepostiory.Setup(x => x.AddUser(It.IsAny<User>())).Returns(Task.CompletedTask);
+
+            var userService = new UserService(mockRepostiory.Object, mockMapper.Object, passwordHasher.Object, null);
+            //ACT
+            var AddedUser = await userService.AddUser(user);
+            //ASSERT
+            Assert.IsNotNull(AddedUser);
+            mockMapper.Verify(x => x.Map<UserResponse>(It.IsAny<User>()), Times.Once());
+            Assert.That(AddedUser.User.Username, !Is.EqualTo(expected.User.Username));
 
         }
     }
